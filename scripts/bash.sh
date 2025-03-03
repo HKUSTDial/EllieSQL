70fe3455014aa5eb3ffa0cbd0fa99b637fcba987
Jiang Runzhi <11810112@mail.sustech.edu.cn>
2025-03-03 07:27:37 +0000

    fix: rename qwen_dpo_train

--- scripts/bash.sh
+++ scripts/bash.sh
@@ -15,4 +15,4 @@ python -m src.classifier.basic_classifier
 
 python -m src.dpo.prepare_dpo_data
 python -m src.dpo.generate_dpo_pairs
-python -m src.dpo.dpo_train_17_18
+python -m src.dpo.qwen_dpo_train
